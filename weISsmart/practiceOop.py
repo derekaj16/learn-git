edef38a7746d875ef64e0db142b55996c110a3f4
derekaj16 <dprodigy@byu.edu>
2022-10-03 12:53:29 -0600

    instructions

---
+++ weISsmart/practiceOop.py
@@ -0,0 +1,11 @@
+#Write a short program that creates a student
+#and a faculty member
+#Get input for the first and last names of each
+#Create a course
+#Assign the course to the student
+#For the student determine if they have a scholarship and store it
+#>= 3.95 = full, > 3.9 = Part, Else None, when this method is called
+#return the full name and a message about the scholarship status
+#For the faculty, ask if they have tenure, return full name and tenure status
+#In the course class, you have a name for the course. This is setup in the constructor
+
